ea99f3ca195fe3e57363ea066f72f1671e029151
taixiang <taixiang@htyg.com.cn>
2018-08-17 18:33:27 +0800

    view 视图

--- blogsite/stu/views.py
+++ blogsite/stu/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import StuImg
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, "stu.html")
+    # forloop.counter
+    imgs = StuImg.objects.all()
+    print(imgs)
+    return render(request, "stu.html", {"imgs": imgs})
