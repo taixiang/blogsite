f3501a14b3f035dc0ee573b1e51b064fd7351b6b
taixiang <taixiang@htyg.com.cn>
2018-08-20 18:18:34 +0800

    图片加载

--- blogsite/stu/urls.py
+++ blogsite/stu/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import index
+from .views import index,moreImg
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^moreImg', moreImg, name='moreImg'),
 ]
--- blogsite/stu/views.py
+++ blogsite/stu/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import StuImg
+from django.core import serializers
+from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -7,5 +10,37 @@ from .models import StuImg
 def index(request):
     # forloop.counter
     imgs = StuImg.objects.all()
-    print(imgs)
-    return render(request, "stu.html", {"imgs": imgs})
+    paginator = Paginator(imgs, 9)
+    page = request.GET.get('page')
+
+    try:
+        data = paginator.page(page)
+    except PageNotAnInteger:
+        data = paginator.page(1)
+    except EmptyPage:
+        data = paginator.page(paginator.num_pages)
+
+    if data.has_next():
+        print("============")
+        has_next = True
+    else:
+        has_next = False
+    return render(request, "stu.html", {"imgs": data, "has_next": has_next})
+
+
+def moreImg(request):
+    page = request.GET.get('page')
+    customer = StuImg.objects.all()
+    paginator = Paginator(customer, 9)
+    try:
+        customer = paginator.page(page)
+    except PageNotAnInteger:
+        customer = paginator.page(1)
+    except EmptyPage:
+        customer = paginator.page(paginator.num_pages)
+    data = serializers.serialize("json", customer)
+    if customer.has_next():
+        data = "{ \"data\":" + data + ",\"page\":" + page + ",\"next\":\"1\" }"
+    else:
+        data = "{ \"data\":" + data + ",\"page\":" + page + ",\"next\":\"\" }"
+    return JsonResponse(data, safe=False)
