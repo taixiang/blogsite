fa1152e928827fc73debd73a918edb9528f23ffa
tx <467220125@qq.com>
2018-10-13 16:26:39 +0800

    统计

--- blogsite/msmm/models.py
+++ blogsite/msmm/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Msmm(models.Model):
+    count = models.IntegerField("数量", default=0)
--- blogsite/msmm/views.py
+++ blogsite/msmm/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Msmm
 
 # Create your views here.
 
 def index(request):
+    counts = Msmm.objects.get_or_create(id=1)[0]
+    counts.count += 1
+    counts.save()
     return render(request, "msmm.html")
