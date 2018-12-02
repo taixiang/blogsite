f84f2e5386cd2bbdc0feb281291aac3d1b798a50
tx <467220125@qq.com>
2018-12-02 23:02:56 +0800

    Html

--- blogsite/coupon/views.py
+++ blogsite/coupon/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Coupon
+
 
 # Create your views here.
 def index(request):
-    return render(request,"coupon.html")
+    coupon_list = Coupon.objects.all()
+    return render(request, "coupon.html", {"coupon_list": coupon_list,"test":"11"})
+
 
 def delete_excel(request):
     return render(request, "coupon.html")
