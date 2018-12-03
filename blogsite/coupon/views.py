92845850aa0194c5e955903cb52b09322c8915bb
taixiang <taixiang@htyg.com.cn>
2018-12-03 18:18:36 +0800

    html

--- blogsite/coupon/views.py
+++ blogsite/coupon/views.py
@@ -1,11 +1,28 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
+
 from .models import Coupon
 
 
 # Create your views here.
 def index(request):
-    coupon_list = Coupon.objects.all()
-    return render(request, "coupon.html", {"coupon_list": coupon_list,"test":"11"})
+    all_data = Coupon.objects.all()
+    paginator = Paginator(all_data, 10)
+    page = request.GET.get('page')
+
+    try:
+        coupon_list = paginator.page(page)
+    except PageNotAnInteger:
+        coupon_list = paginator.page(1)
+    except EmptyPage:
+        coupon_list = paginator.page(paginator.num_pages)
+
+    if coupon_list.has_next():
+        has_next = True
+    else:
+        has_next = False
+
+    return render(request, "coupon.html", {"coupon_list": coupon_list, "has_next": has_next, "q": 0})
 
 
 def delete_excel(request):
