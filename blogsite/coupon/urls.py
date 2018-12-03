192a4a5af84a1031c9654281974597e0d40f06cd
tx <467220125@qq.com>
2018-12-03 22:54:45 +0800

    header

--- blogsite/coupon/urls.py
+++ blogsite/coupon/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, type_list
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'(?P<type>.*)/$', type_list, name='type_list'),
 
 ]
--- blogsite/coupon/views.py
+++ blogsite/coupon/views.py
@@ -22,7 +22,29 @@ def index(request):
     else:
         has_next = False
 
-    return render(request, "coupon.html", {"coupon_list": coupon_list, "has_next": has_next, "q": 0})
+    return render(request, "coupon.html", {"coupon_list": coupon_list, "has_next": has_next})
+
+
+def type_list(request, type):
+    print("========")
+    print(type)
+    all_data = Coupon.objects.all()
+    paginator = Paginator(all_data, 10)
+    page = request.GET.get('page')
+
+    try:
+        coupon_list = paginator.page(page)
+    except PageNotAnInteger:
+        coupon_list = paginator.page(1)
+    except EmptyPage:
+        coupon_list = paginator.page(paginator.num_pages)
+
+    if coupon_list.has_next():
+        has_next = True
+    else:
+        has_next = False
+
+    return render(request, "coupon.html", {"coupon_list": coupon_list, "has_next": has_next, "type": type})
 
 
 def delete_excel(request):
